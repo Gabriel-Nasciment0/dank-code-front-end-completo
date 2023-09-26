6ac60634f127eab16b6e412e1ca46bebf3a6225d
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-09-26 09:41:59 -0300

    .

---
+++ modulo 09 javascript/09 mais sobre laçoes e repetição/script.js
@@ -0,0 +1,18 @@
+var n = 0;
+// continue
+// break
+/*
+for (var i = 0; i <10; i++){
+    if(==2){
+        break;
+    }
+    console.log(i);
+}
+*/
+while (n < 10) {
+    console.log(n);
+    if (n == 5) {
+        break;
+    }
+    n++;
+}
---
+++ modulo 09 javascript/10 switch e case/script.js
@@ -0,0 +1,9 @@
+var nome = "gabriel";
+switch (nome) {
+    case "joao":
+        break;
+
+    case " gabriel":
+        console.log("acerto");
+        break;
+}
