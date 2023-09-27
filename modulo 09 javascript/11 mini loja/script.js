5585943010474f391900b243a33bffc663011171
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-09-27 09:04:12 -0300

    .

---
+++ modulo 09 javascript/11 mini loja/script.js
@@ -0,0 +1,14 @@
+const itens = {
+    id: 0,
+    nome: "camiseta",
+    img: "imagens/download.png",
+    quantidade: 0,
+    id: 1,
+    nome: "camiseta",
+    img: "imagens/download.png",
+    quantidade: 0,
+    id: 2,
+    nome: "camiseta",
+    img: "imagens/download.png",
+    quantidade: 0,
+};
