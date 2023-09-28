be5fb68d8f7e52b41fae66407475741407d96bf7
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-09-28 15:17:58 -0300

    .

--- modulo 10 jquery/02 manipulando/js/functions.js
+++ modulo 10 jquery/02 manipulando/js/functions.js
@@ -1,3 +1,7 @@
 $(function () {
-    alert("ola mundo");
+    $("#teste").css("background", "gray");
+    $(".artigo01").css("padding", "20px");
+    setTimeout(function () {
+        $(".artigo01 > p").css("color", "red");
+    }, 2000);
 });
