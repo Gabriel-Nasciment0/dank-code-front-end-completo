d06b67f08d4976eb24c073fedea8218101f86594
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-11-27 14:52:03 -0300

    .

---
+++ modulo 18 javaScript avancado/01 introdução/js/script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var el = document.getElementById("mainText");
+    el.innerHTML = el.innerHTML + ", Gabriel";
+    el.style.background = "red";
+    el.style.margin = "30px";
+    el.style.border = "3px solid blue";
+};
