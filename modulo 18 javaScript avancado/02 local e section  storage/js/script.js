1c5924e32d994fa0bf50ba89cfe4290786e0b873
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-11-29 14:31:40 -0300

    ;

---
+++ modulo 18 javaScript avancado/02 local e section  storage/js/script.js
@@ -0,0 +1,14 @@
+if (typeof Storage !== "undefined") {
+    console.log("o navegador e compativel");
+} else {
+    console.log("o navegador não e compativel");
+}
+
+function initialize() {
+    if (sessionStorage.getItem("nome") !== null) {
+        alert("Olá, " + sessionStorage.nome + ", Seja bem vindo de volta");
+    } else {
+        alert("bem vindo essa e sua primeira visita no nosso site");
+        sessionStorage.nome = "gabriel";
+    }
+}
