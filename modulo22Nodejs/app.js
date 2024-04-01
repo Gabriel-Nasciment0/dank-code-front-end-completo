d065f043e7afc0fe9f391a4bd36c3bc88e260119
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-04-01 09:45:45 -0300

    e

--- modulo22Nodejs/app.js
+++ modulo22Nodejs/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const path = require("path")
+const path = require("path");
 
 app.listen("3000", () => {
     console.log("servidor rodando");
 });
+//Body Parser
+app.set("view engine", "ejs");
+app.set("vielw", path.join("__dirname", "views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function(req ,res){
+    res.render('index',{})
+})
