06a03600d2422beacfa0cbd79ad22b94d64a40c3
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-04-02 16:11:40 -0300

    .

--- modulo22 nodejs/app.js
+++ modulo22 nodejs/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
+const mysql = require("mysql");
 
 app.listen("3000", () => {
     console.log("servidor rodando");
@@ -14,9 +15,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//conecxao com o banco
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node",
+});
+db.connect(function (err) {
+    if (err) {
+        console.log("Erro no banco de dados");
+    }
+});
+
 app.get("/", function (req, res) {
     res.render("index", {});
-});
-app.get("/sobre", function (req, res) {
-    res.render("sobre", {});
 });
